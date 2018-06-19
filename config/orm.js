4cf3189606350e7b31deeba327a46493d04cd364
Linh Tong <linhthitong@gmail.com>
2018-06-19 15:04:05 -0700

    adjust connect export

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,6 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
+console.log(connection)
 
 //export orm for use by the model
 var orm = module.exports = {};
